c44e87fd87465e5962357f68c7e403228ffe757b
ufoo4 <chisa.doe@gmail.com>
2024-04-17 18:56:23 +0700

    array

--- 03basic/array.go
+++ 03basic/array.go
@@ -8,6 +8,13 @@ func main() {
 
 	l := len(list) // 0
 	c := cap(list) // 0
+	l2 := len(list2)
+	c2 := cap(list2)
+
+	_ = l
+	_ = c
+	_ = l2
+	_ = c2
 
 	list = []int64{1, 2, 3, 4, 5} // [1, 2, 3, 4, 5]
 	l = len(list)                 // 5
@@ -26,6 +33,16 @@ func main() {
 	list = []int64{1, 2, 3, 4} // [1,2,3,4]
 	list = append(list, 5)     // [1,2,3,4,5]
 
-	fmt.Println(list2, l, c)
+	list = make([]int64, 0, 3)
+	list = append(list, 1) // [1] len:1 cap:3
+	list = append(list, 2) // [1,2] len:2 cap:3
+	list = append(list, 3) // [1,2,3] len:3 cap:3
+	list = append(list, 4) // [1,2,3,4] len:4 cap:6
+	list = append(list, 5) // [1,2,3,4,5] len:5 cap:6
+
+	l = len(list)
+	c = cap(list)
+
+	fmt.Println(list, l, c)
 
 }
