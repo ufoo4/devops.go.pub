67a8dc8aec4f6439042fa73d6976320bae9259ad
ufoo4 <chisa.doe@gmail.com>
2024-05-05 15:21:59 +0700

    complete 03

--- 03-basic/practice/03.06.03/main.go
+++ 03-basic/practice/03.06.03/main.go
@@ -1,1 +1,16 @@
-package _3_06_03
+package main
+
+import "fmt"
+
+func sort(sls []string) bool {
+	for i := 1; i < len(sls); i++ {
+		if sls[i] < sls[i-1] {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	sls := []string{"Close", "Danke", "Ferder", "Liben"}
+	fmt.Println(sort(sls))
+}
